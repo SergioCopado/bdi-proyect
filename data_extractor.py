d5dc244712d0af806615ccdb206736fc3fabfbc2
cbarbero <114517037+CBM2w@users.noreply.github.com>
2024-03-30 08:45:00 +0100

    Update data_extractor.py

--- data_extractor.py
+++ data_extractor.py
@@ -11,46 +11,52 @@ def limpiar_titulo(titulo):
     titulo_limpio = re.sub(r'\s+', ' ', titulo_limpio).strip()
     return titulo_limpio
 
-def buscar_libros(parametros):
+def buscar_libros(parametros_busqueda):
     url = 'http://openlibrary.org/search.json'
     resultados_totales = 0
+    libros_encontrados = []
 
     try:
         # Crear la carpeta json si no existe
         if not os.path.exists('json'):
             os.makedirs('json')
-            
-        # Realizar la primera solicitud para obtener el número total de resultados
-        response = requests.get(url, params=parametros)
-        data = response.json()
 
-        if response.status_code == 200:
-            resultados_totales = data['numFound']
-            print(f"Se encontraron un total de {resultados_totales} resultados.")
+        for genero in parametros_busqueda:
+            parametros = {'q': genero}
+            # Realizar la solicitud para obtener el número total de resultados
+            response = requests.get(url, params=parametros)
+            data = response.json()
 
-            # Iterar sobre todas las páginas de resultados
-            for pagina in range(1, (resultados_totales // 100) + 2):  # + 2 para asegurarse de obtener la última página
-                parametros['page'] = pagina
-                response = requests.get(url, params=parametros)
-                data = response.json()
+            if response.status_code == 200:
+                resultados_totales += data['numFound']
+                print(f"Se encontraron un total de {data['numFound']} resultados para el género {genero}.")
 
-                if response.status_code == 200:
-                    if data['docs']:
-                        for libro in data['docs']:
-                            titulo = libro.get('title', 'No disponible')
-                            titulo_limpio = limpiar_titulo(titulo)
-                            with open(f'/json/{titulo_limpio}.json', 'w', encoding='utf-8') as file:
-                                json.dump(libro, file, indent=4)
+                # Iterar sobre todas las páginas de resultados
+                for pagina in range(1, (data['numFound'] // 100) + 2):  # + 2 para asegurarse de obtener la última página
+                    parametros['page'] = pagina
+                    response = requests.get(url, params=parametros)
+                    data = response.json()
+
+                    if response.status_code == 200:
+                        if data['docs']:
+                            for libro in data['docs']:
+                                titulo = libro.get('title', 'No disponible')
+                                titulo_limpio = limpiar_titulo(titulo)
+                                with open(f'json/{titulo_limpio}.json', 'w', encoding='utf-8') as file:
+                                    json.dump(libro, file, indent=4)
+                                    libros_encontrados.append(libro)
+                        else:
+                            print(f"No se encontraron libros en la página {pagina} para el género {genero}.")
                     else:
-                        print(f"No se encontraron libros en la página {pagina}.")
-                else:
-                    print(f"No se pudo obtener la página {pagina} de resultados.")
-        else:
-            print("No se encontraron libros con los criterios especificados.")
+                        print(f"No se pudo obtener la página {pagina} de resultados para el género {genero}.")
+
+        print(f"Se encontraron un total de {resultados_totales} resultados para todos los géneros proporcionados.")
+
+        return libros_encontrados
 
     except Exception as e:
         print(f"Ocurrió un error: {e}")
 
 # Ejemplo de uso
-parametros_busqueda = {'q': 'horror'}
+parametros_busqueda = ['science fiction', 'mystery', 'romance', 'action', 'fantasy', 'thriller', 'horror']
 buscar_libros(parametros_busqueda)
