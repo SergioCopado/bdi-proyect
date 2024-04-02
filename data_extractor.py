266f6f93bf1408c5f9b93e0658e3045a2d14c06c
cbarbero <114517037+CBM2w@users.noreply.github.com>
2024-04-02 11:35:47 +0200

    Update data_extractor.py
    Script que funciona y se pueden ampliar los géneros literarios, pero ya se obtienen alrededor de 200.000 archivos.

--- data_extractor.py
+++ data_extractor.py
@@ -3,40 +3,47 @@ import json
 import requests
 import re
 
-def limpiar_titulo(titulo):
+def limpiar_titulo(titulo, longitud_maxima=100):
     # Reemplazar espacios con barras bajas
     titulo_limpio = titulo.replace(' ', '_')
     # Eliminar caracteres especiales y espacios adicionales del título
     titulo_limpio = re.sub(r'[^\w\s]', '', titulo_limpio)
     titulo_limpio = re.sub(r'\s+', ' ', titulo_limpio).strip()
+    # Truncar el título si excede la longitud máxima
+    if len(titulo_limpio) > longitud_maxima:
+        titulo_limpio = titulo_limpio[:longitud_maxima]
     return titulo_limpio
 
 def buscar_libros(parametros_busqueda):
     url = 'http://openlibrary.org/search.json'
-    resultados_totales = 0
     libros_encontrados = []
 
     try:
-        # Crear la carpeta json si no existe
+        # Creamos la carpeta json si no existe
         if not os.path.exists('json'):
             os.makedirs('json')
 
         for genero in parametros_busqueda:
+            # Pasamos como parámetros los géneros literarios
             parametros = {'q': genero}
-            # Realizar la solicitud para obtener el número total de resultados
+
+            # Realizamos una petición a la url de la página
             response = requests.get(url, params=parametros)
             data = response.json()
 
             if response.status_code == 200:
-                resultados_totales += data['numFound']
-                print(f"Se encontraron un total de {data['numFound']} resultados para el género {genero}.")
-
-                # Iterar sobre todas las páginas de resultados
-                for pagina in range(1, (data['numFound'] // 100) + 2):  # + 2 para asegurarse de obtener la última página
+                # Iteramos sobre todas las páginas de resultados
+                for pagina in range(1, (data['numFound'] // 100) + 2):  
                     parametros['page'] = pagina
                     response = requests.get(url, params=parametros)
-                    data = response.json()
 
+                    try:
+                        data = response.json()
+                    except json.JSONDecodeError as e:
+                        print(f"Error al decodificar JSON en la página {pagina} para el género {genero}: {e}")
+                        continue
+                    
+                    # Cogemos los archivos json y los guardamos con el nombre del título
                     if response.status_code == 200:
                         if data['docs']:
                             for libro in data['docs']:
@@ -45,18 +52,16 @@ def buscar_libros(parametros_busqueda):
                                 with open(f'json/{titulo_limpio}.json', 'w', encoding='utf-8') as file:
                                     json.dump(libro, file, indent=4)
                                     libros_encontrados.append(libro)
-                        else:
-                            print(f"No se encontraron libros en la página {pagina} para el género {genero}.")
-                    else:
-                        print(f"No se pudo obtener la página {pagina} de resultados para el género {genero}.")
 
-        print(f"Se encontraron un total de {resultados_totales} resultados para todos los géneros proporcionados.")
+        # Contamos el número de archivos descargados
+        numero_archivos_descargados = len(os.listdir('json'))
+        print(f"Se encontraron un total de {numero_archivos_descargados} archivos en la carpeta 'json'.")
 
         return libros_encontrados
 
-    except Exception as e:
-        print(f"Ocurrió un error: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error en la solicitud: {e}")
 
-# Ejemplo de uso
-parametros_busqueda = ['science fiction', 'mystery', 'romance', 'action', 'fantasy', 'thriller', 'horror']
+# Ejemplo de uso con distintos géneros literarios
+parametros_busqueda = ['science fiction', 'action', 'thriller', 'horror']
 buscar_libros(parametros_busqueda)
